82010a7cc99bf1ce416c7acfff5159f4a6e768fa
lukaszdutka <lukaszdutka2@gmail.com>
2017-06-12 16:26:31 +0200

    -

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,37 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
 /**
  * Created by Dasi on 2017-06-12.
  */
-public class Game {
+public class Game extends Application {
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Circle circ = new Circle(40, 40, 30);
+        Group root = new Group(circ);
+        Scene scene = new Scene(root, 400, 300);
 
-    public void start(){
+        stage.setTitle("Snek gme.");
+        stage.setScene(scene);
+        stage.show();
 
     }
 
+    public Game() {
+        super();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
 }
