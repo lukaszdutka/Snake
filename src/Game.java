0314a9ee06b90501bdc51933c94030a0a220383b
lukaszdutka <lukaszdutka2@gmail.com>
2017-06-29 23:00:08 +0200

    First big update of Snake

--- src/Game.java
+++ src/Game.java
@@ -1,37 +1,119 @@
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-
 /**
- * Created by Dasi on 2017-06-12.
+ * Creator: Dasi
+ * Date: 2017-02-06
+ * Description:
  */
-public class Game extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Circle circ = new Circle(40, 40, 30);
-        Group root = new Group(circ);
-        Scene scene = new Scene(root, 400, 300);
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.util.Random;
 
-        stage.setTitle("Snek gme.");
-        stage.setScene(scene);
-        stage.show();
+public class Game extends Canvas implements Runnable {
 
-    }
+    private static final long serialVersionUID = 1L;
+
+    public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+
+    private Thread thread;
+    private boolean running = false;
+
+    private Random r;
+    private Handler handler;
+    private HUD hud;
 
     public Game() {
-        super();
+        handler = new Handler();
+        this.addKeyListener(new KeyInput(handler));
+
+        new Window(WIDTH, HEIGHT, "Let's Build a Game!", this);
+
+        hud = new HUD();
+
+        r = new Random();
+
+        handler.addObject(new Player(r.nextInt(WIDTH/2), r.nextInt(HEIGHT/2), ID.Player, handler, WIDTH, HEIGHT));
+        handler.addObject(new Point(r.nextInt(WIDTH)/16 * 16, r.nextInt(HEIGHT)/16 * 16, ID.Point, handler));
+
     }
 
-    @Override
-    public void init() throws Exception {
-        super.init();
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
     }
 
-    @Override
-    public void stop() throws Exception {
-        super.stop();
+    public synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                tick();
+                delta--;
+            }
+            if (running)
+                render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick() {
+        handler.tick();
+        hud.tick();
+    }
+
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        handler.render(g);
+
+        hud.render(g);
+
+        g.dispose();
+        bs.show();
+    }
+
+    public static int clamp(int var, int min, int max){
+        if(var >= max)
+            return var = max;
+        if(var <= min)
+            return var = min;
+        else
+            return var;
+    }
+
+    public static void main(String args[]) {
+        new Game();
     }
 }
